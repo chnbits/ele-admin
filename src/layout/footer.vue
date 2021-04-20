3e35ea878402779560fd3b8836aa8d1015c7843c
chnbits <riskcn@163.com>
2021-04-20 15:58:27 +0800

    项目上传

---
+++ src/layout/footer.vue
@@ -0,0 +1,33 @@
+<!-- 页脚 -->
+<template>
+  <div class="ele-text-center" style="padding: 16px 0;">
+    <div>
+      <a
+        class="ele-text-secondary"
+        href="https://eleadmin.com"
+        target="_blank">官网
+      </a>
+      <em/>
+      <a
+        class="ele-text-secondary"
+        href="https://eleadmin.com/doc/eleadmin/"
+        target="_blank">文档
+      </a>
+      <em/>
+      <a
+        class="ele-text-secondary"
+        href="https://eleadmin.com/goods/8"
+        target="_blank">授权
+      </a>
+    </div>
+    <div class="ele-text-secondary" style="margin-top: 8px;">
+      Copyright © 2021 武汉易云智科技有限公司
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EleFooter'
+}
+</script>
---
+++ src/views/extension/more/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="ele-body">
+    <more-tag/>
+    <more-dot/>
+    <more-alert/>
+    <more-steps/>
+    <more-menu/>
+    <more-message/>
+    <more-empty/>
+    <more-city-select/>
+    <more-cropper/>
+    <more-count-up/>
+    <more-qrcode/>
+  </div>
+</template>
+
+<script>
+import MoreTag from './more-tag';
+import MoreDot from './more-dot';
+import MoreAlert from './more-alert';
+import MoreSteps from './more-steps';
+import MoreMenu from './more-menu';
+import MoreMessage from './more-message';
+import MoreEmpty from './more-empty';
+import MoreCitySelect from './more-city-select';
+import MoreCropper from './more-cropper';
+import MoreCountUp from './more-count-up';
+import MoreQrcode from './more-qrcode';
+
+export default {
+  name: 'ExtensionMore',
+  components: {
+    MoreTag,
+    MoreDot,
+    MoreAlert,
+    MoreSteps,
+    MoreMenu,
+    MoreMessage,
+    MoreEmpty,
+    MoreCitySelect,
+    MoreCropper,
+    MoreCountUp,
+    MoreQrcode
+  }
+}
+</script>
+
+<style scoped>
+</style>
